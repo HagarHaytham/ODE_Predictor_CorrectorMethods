1df47865c8a44e998c8c4a57793b0215572f5ed7
Hagar Haytham <hagar.bahy97@eng-st.cu.edu.eg>
2018-12-14 03:21:30 +0200

    updated eval

--- pccommon.py
+++ pccommon.py
@@ -4,7 +4,6 @@ Created on Sat Dec  8 01:53:29 2018
 
 @author: hagar
 """
-
 from math import *
 
 class Point:
@@ -15,7 +14,11 @@ class Point:
     
 def GetF(x,y,equation):
     #should add all kind of functions that can be entered ,, cos ..... etc
-    f=eval(equation,{'__builtins__': None},{'cos':cos,'sin':sin,'x': x,'y':y,'exp':exp, 'sqrt': sqrt})
+    f=eval(equation,{'__builtins__': None},
+    {'x': x,'y':y,'cos':cos,'sin':sin,'tan':tan,'acos':acos,'asin':asin,
+     'atan':atan,'exp':exp,'pow':pow, 'sqrt': sqrt,'log10':log10,'log':log, #ln
+     'fabs':fabs,'radians':radians,'degrees':degrees,'cosh':cosh,'acosh':acosh,
+     'sinh':sinh,'asinh':asinh,'tanh':tanh,'atanh':atanh,'pi':pi,'e':e})
     return f
         
 def StoppingCriteria (myApproxError,approxerror,i,itterations):

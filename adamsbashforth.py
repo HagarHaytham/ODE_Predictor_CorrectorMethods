976691bf719568097496537d6ae7dd4edd8a6ca0
sara.a.cufe@gmail.com <sara.a.cufe@gmail.com>
2018-12-14 18:48:31 +0200

    implementing adams bashfoth

--- adamsbashforth.py
+++ adamsbashforth.py
@@ -11,16 +11,14 @@ import numpy as np
 def GetF(x,y,equation):# gets F some how from the equation 
     #should add all kind of functions that can be entered ,, cos ..... etc
     f=eval(equation,{'__builtins__': None}, {'x': x,'y':y,'exp':exp, 'sqrt': sqrt})
-    
     #print("printing f "+str(f))
     return f
 
-#assume x after the last point in table
+#assume x after the last point in table ##error
 def AdamsBashforth(xpoints,ypoints,equation,x,error):
-    points=[]
-    f=[]
+    points=[] #contains all given points
+    f=[] #all calculated f(x,y)
     y0 = 0
-    y = 0
     matrix= np.zeros((5,5))
     #calculate f
     for i in range (len(xpoints)):
@@ -32,7 +30,7 @@ def AdamsBashforth(xpoints,ypoints,equation,x,error):
     #step
     h = abs(points[1].x-points[0].x)
     matrix[0] = f
-    
+    #Fill the table with intial values 
     for j in range (1,len(xpoints)):
         for k in range (0,len(xpoints)-j):
             matrix[j][k] = matrix[j-1][k+1]-matrix[j-1][k]
@@ -41,20 +39,19 @@ def AdamsBashforth(xpoints,ypoints,equation,x,error):
         print("matrix1: " , matrix)
         if i == 0:
             y0 = points[-1].y
-            y = y0+h*(points[-1].f+0.5*matrix[1][len(xpoints)-2]+(5/12)*matrix[2][len(xpoints)-3]+ (3/8)*matrix[3][len(xpoints)-4]+ (251/720)*matrix[4][len(xpoints)-5])
+            yPredict = y0+h*(points[-1].f+0.5*matrix[1][len(xpoints)-2]+(5/12)*matrix[2][len(xpoints)-3]+ (3/8)*matrix[3][len(xpoints)-4]+ (251/720)*matrix[4][len(xpoints)-5])
             #put y in the table
             print("y: ", points[-1].f)
-            matrix[0][len(xpoints)] = GetF(x,y,equation)
+            matrix[0][len(xpoints)] = GetF(x,yPredict,equation)
             
         #calculate all new values in the table
         for j in range (0,len(xpoints)-1):
                 matrix[j+1][-j-2] = matrix[j][-j-1]-matrix[j][-j-2]
                 
         print("matrix3: " , matrix)
-        #Assume f1 hya l f kol marra bt7sbha ??
-        y = y0+h*(matrix[0][len(xpoints)-1]-0.5*matrix[1][len(xpoints)-2]-(1/12)*matrix[2][len(xpoints)-3]-(1/24)*matrix[3][len(xpoints)-5]-(19/720)*matrix[4][len(xpoints)-5])
+        yCorrect = y0+h*(matrix[0][len(xpoints)-1]-0.5*matrix[1][len(xpoints)-2]-(1/12)*matrix[2][len(xpoints)-3]-(1/24)*matrix[3][len(xpoints)-5]-(19/720)*matrix[4][len(xpoints)-5])
 
-        matrix[0][len(xpoints)] = GetF(x,y,equation)
+        matrix[0][len(xpoints)] = GetF(x,yCorrect,equation)
         
             
 AdamsBashforth([-0.1,0,0.1,0.2],[1.0047,1,1.0053,1.0229],"x*y+x**2",0.3,0)
